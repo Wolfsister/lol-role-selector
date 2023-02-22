393b87a4dace31a1ff760e41c7a8f1f1bdf587c4
Denis CHENESSEAU <denis.chenesseau@gmail.com>
2023-02-22 11:31:41 +0000

    feat: add some spacing in home page, switch to column when mobile

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
+import {useState} from 'react'
 import './App.css'
 import {
     Button,
@@ -19,87 +18,95 @@ import {givePlayersRoles} from "./utils/roleCalculator";
 import {ROLES} from "./constants/Roles";
 
 function App() {
-  const [players, setPlayers] = useState<Player[]>([]);
-  const [newPlayerName, setNewPlayerName] = useState<string>('');
-  const [openDialogPlayersRoles, setOpenDialogPlayersRoles] = useState<boolean>(false);
+    const [players, setPlayers] = useState<Player[]>([]);
+    const [newPlayerName, setNewPlayerName] = useState<string>('');
+    const [openDialogPlayersRoles, setOpenDialogPlayersRoles] = useState<boolean>(false);
 
-  const onClickRole = (clickedRole: string, playerId: number) => {
-      const updatedPlayers = players.map((player) => {
-          if (player.id === playerId) {
-              const updatedAcceptedRoles = player.acceptedRoles.includes(clickedRole) ? player.acceptedRoles.filter((acceptedRole) => acceptedRole !== clickedRole) : [...player.acceptedRoles, clickedRole];
-              return {...player, acceptedRoles: updatedAcceptedRoles};
-          }
-          return player;
-      })
-      setPlayers(updatedPlayers);
-  }
+    const onClickRole = (clickedRole: string, playerId: number) => {
+        const updatedPlayers = players.map((player) => {
+            if (player.id === playerId) {
+                const updatedAcceptedRoles = player.acceptedRoles.includes(clickedRole) ? player.acceptedRoles.filter((acceptedRole) => acceptedRole !== clickedRole) : [...player.acceptedRoles, clickedRole];
+                return {...player, acceptedRoles: updatedAcceptedRoles};
+            }
+            return player;
+        })
+        setPlayers(updatedPlayers);
+    }
 
-  const onClearPlayer = (playerId: number) => {
+    const onClearPlayer = (playerId: number) => {
 
-      const updatedPlayers = players.filter((player) => {
-          return player.id !== playerId;
-      })
-      setPlayers(updatedPlayers);
-  }
+        const updatedPlayers = players.filter((player) => {
+            return player.id !== playerId;
+        })
+        setPlayers(updatedPlayers);
+    }
 
-  const onClickRoleCalculator = () => {
-      const playersWithRoles = givePlayersRoles(players);
-      if (playersWithRoles) {
-          setPlayers(playersWithRoles)
-          setOpenDialogPlayersRoles(true);
-      }
-  }
+    const onClickRoleCalculator = () => {
+        const playersWithRoles = givePlayersRoles(players);
+        if (playersWithRoles) {
+            setPlayers(playersWithRoles)
+            setOpenDialogPlayersRoles(true);
+        }
+    }
 
-  const addPlayer = () => {
-      if (newPlayerName.length === 0) {
-          alert('Merci de choisir un nom au préalable.');
-          return;
-      }
-      setPlayers([...players, {
-          id: players.length + 1,
-          name: newPlayerName,
-          acceptedRoles: ROLES,
-          givenRole: "",
-          randomHash: Math.floor(Math.random()*2000),
-      }])
-      setNewPlayerName('');
-  }
+    const addPlayer = () => {
+        if (newPlayerName.length === 0) {
+            alert('Merci de choisir un nom au préalable.');
+            return;
+        }
+        setPlayers([...players, {
+            id: players.length + 1,
+            name: newPlayerName,
+            acceptedRoles: ROLES,
+            givenRole: "",
+            randomHash: Math.floor(Math.random() * 2000),
+        }])
+        setNewPlayerName('');
+    }
 
-  return (
-    <div className="App">
-        <Typography variant="h2" component="h1" sx={{textAlign: 'center'}}>Bienvenue sur votre sélecteur de rôle préféré !</Typography>
-        <Typography variant="h3" sx={{fontSize: 'medium', textAlign: 'center'}}> (l'unique ? j'ai pas cherché)</Typography>
-        <TextField
-            id="outlined-controlled"
-            label="Nom du nouveau joueur"
-            value={newPlayerName}
-            onChange={(event: React.ChangeEvent<HTMLInputElement>) => {
-                setNewPlayerName(event.target.value);
-            }}
-        />
-        <Button disabled={players.length === 5 } onClick={() => addPlayer()}>Ajouter un joueur</Button>
-        <Stack flexDirection={"row"}>
-            {players.map((player) => (
-                <PlayerCard onClickRole={onClickRole} onClearPlayer={onClearPlayer} player={player} key={player.id}/>
-            ))}
-        </Stack>
-        <Button variant="contained" disabled={players.length === 0} onClick={onClickRoleCalculator}>Choisir les rôles</Button>
-
-        <Dialog open={openDialogPlayersRoles} onClose={() => setOpenDialogPlayersRoles(false)}>
-            <DialogTitle>Composition trouvée !</DialogTitle>
-            <DialogContent>
-                <List>
+    return (
+        <div className="App">
+            <Stack gap={"10px"}>
+            <Typography variant="h2" component="h1" sx={{textAlign: 'center'}}>Bienvenue sur votre sélecteur de rôle
+                préféré !</Typography>
+            <Typography variant="h3" sx={{fontSize: 'medium', textAlign: 'center'}}> (l'unique ? j'ai pas
+                cherché)</Typography>
+            <Stack alignSelf={"center"} flexDirection={"row"} gap={2}><TextField
+                id="outlined-controlled"
+                label="Nom du nouveau joueur"
+                value={newPlayerName}
+                onChange={(event: React.ChangeEvent<HTMLInputElement>) => {
+                    setNewPlayerName(event.target.value);
+                }}
+            />
+            <Button disabled={players.length === 5} onClick={() => addPlayer()}>Ajouter un joueur</Button>
+            </Stack>
+            <Stack gap={2} alignSelf={"center"} flexDirection={{xs: "column", md: "row"}}>
                 {players.map((player) => (
-                    <ListItem key={player.id}>
-                        <ListItemText secondary={player.givenRole}>{player.name}</ListItemText>
-                    </ListItem>
+                    <PlayerCard onClickRole={onClickRole} onClearPlayer={onClearPlayer} player={player}
+                                key={player.id}/>
                 ))}
-                </List>
-            </DialogContent>
-        </Dialog>
+            </Stack>
+            {players.length > 0 &&
+                <Button sx={{width: "fit-content", alignSelf: "center"}} variant="contained" disabled={players.length === 0} onClick={onClickRoleCalculator}>Choisir les
+                    rôles</Button>
+            }
 
-    </div>
-  )
+            <Dialog open={openDialogPlayersRoles} onClose={() => setOpenDialogPlayersRoles(false)}>
+                <DialogTitle>Composition trouvée !</DialogTitle>
+                <DialogContent>
+                    <List>
+                        {players.map((player) => (
+                            <ListItem key={player.id}>
+                                <ListItemText secondary={player.givenRole}>{player.name}</ListItemText>
+                            </ListItem>
+                        ))}
+                    </List>
+                </DialogContent>
+            </Dialog>
+            </Stack>
+        </div>
+    )
 }
 
 export default App
